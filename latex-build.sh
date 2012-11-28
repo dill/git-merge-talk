d26af08fa4b7d16ec783d9aebad31b5e05a3f965
David Lawrence Miller <dave@ninepointeightone.net>
2012-11-28 11:45:45 -0500

    updated build

--- latex-build.sh
+++ latex-build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
+# build slidy presentation
 pandoc -s -S -i -t slidy --mathjax talk.md -o talk.html
-#pandoc -s -i talk.md -o talk.tex --variable=slidy-url:Slidy2 -t beamer -S --mathjax=mathjax/MathJax.js?config=TeX-AMS-MML_HTMLorMML
-#pandoc -s -i talk.md -o talk.html -t slidy -S --mathjax=mathjax/MathJax.js?config=TeX-AMS-MML_HTMLorMML
-#--mathjax=mathjax/MathJax.js 
+# build tex for beamer
+pandoc -s -i talk.md -o talk.tex -t beamer -S
